cb60a70169947143ccf8fc8437e45d08f6fa7330
Mateo Vidal <jvidalba@gmail.com>
2014-01-06 00:55:56 -0500

    table view controller

---
+++ Baccus/WineryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WineryTableViewController.h
+//  Baccus
+//
+//  Created by Mateo Vidal on 6/01/14.
+//  Copyright (c) 2014 Mateo Vidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WineryTableViewController : UITableViewController
+
+@end
