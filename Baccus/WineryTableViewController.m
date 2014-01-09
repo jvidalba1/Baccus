692866bac20e2f4efdc7d46e3e19d045bd5f8b76
Mateo Vidal <jvidalba@gmail.com>
2014-01-08 23:37:42 -0500

    New methods to develop last wine selected functionality (NSUserDefaults)
     Using data persistence level one (NSUserDefaults)

--- Baccus/WineryTableViewController.m
+++ Baccus/WineryTableViewController.m
@@ -6,8 +6,11 @@
 //  Copyright (c) 2014 Mateo Vidal. All rights reserved.
 //
 
-#import "WineryTableViewController.h"
+#import "WineModel.h"
 #import "WineViewController.h"
+#import "WebViewController.h"
+#import "WineryModel.h"
+#import "WineryTableViewController.h"
 
 @interface WineryTableViewController ()
 
@@ -15,7 +18,8 @@
 
 @implementation WineryTableViewController
 
--(id) initWithModel:(WineryModel *) aModel style:(UITableViewStyle) aStyle{
+-(id) initWithModel:(WineryModel *) aModel
+              style:(UITableViewStyle) aStyle{
     
     if (self = [super initWithStyle:aStyle]) {
         _model = aModel;
@@ -189,8 +193,26 @@
  */
 
 #pragma mark - Table View Delegate
--(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+-(void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
+    // Escogemos el vino seleccionado
+    WineModel *wine = [self wineForIndexPath:indexPath];
+    
+    // Avisar al delegado
+    [self.delegate wineryTableViewController:self didSelecteWine:wine];
+    
+    //Enviar notificacion
+    NSNotification *n = [NSNotification notificationWithName:NEW_WINE_NOTIFICATION_NAME object:self userInfo:@{WINE_KEY: wine}];
+    
+    [[NSNotificationCenter defaultCenter] postNotification:n];
+    
+    // Guardar el ultimo vino seleccionado
+    [self saveLastSelectedWineAtSection:indexPath.section row:indexPath.row];
+    
+    
+    /*
     // Suponemos que estamos en n navigation controller
     
     // Averiguamos de que vino se trata
@@ -226,7 +248,89 @@
                                                       object:self
                                                     userInfo:@{WINE_KEY: wine}];
     
-    [[NSNotificationCenter defaultCenter]postNotification:n];
+    [[NSNotificatioCenter defaultCenter]postNotification:n];
+     */
+}
+
+#pragma mark - NSUserDefaults
+
+- (NSDictionary *) setDefaults{
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    // Creo el diccionario con las claves que tengo definidas
+    // Por defecto, mostramos el primero de los tintos
+    NSDictionary *defaultWineCoords = @{SECTION_KEY : @(RED_WINE_SECTION), ROW_KEY : @0};
+    
+    // lo asignamos
+    [defaults setObject:defaultWineCoords
+                 forKey:LAST_WINE_KEY];
+    
+    // Guardamos por si las moscas
+    [defaults synchronize];
+    
+    return defaultWineCoords;
+}
+
+// Guardar las coordenadas del ultimo vino seleccionado
+- (void)saveLastSelectedWineAtSection:(NSInteger)section
+                                   row:(NSInteger)row{
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:@{SECTION_KEY : @(section), ROW_KEY : @(row)}
+                 forKey:LAST_WINE_KEY];
+                          
+    [defaults synchronize];
+}
+
+- (WineModel *)lastSelectedWine{
+    
+    NSIndexPath *indexPath = nil;
+    NSDictionary *coords = nil; // Guardar coordenadas del vino
+    
+    /* Obtener las coordenadas creando una instancia 
+     * NSUserDefaults y le mando el 
+     * mensaje objectForKey (constante)
+     */
+    coords = [[NSUserDefaults standardUserDefaults] objectForKey:LAST_WINE_KEY];
+    
+    if (coords == nil) {
+        /*
+         * No hay nada bajo la clave LAST_WINE_KEY,
+         * esto quiere decir que es la primera vez que se llama a la app
+         * Ponemos un valor por defecto: el primero de los tintos
+         */
+        coords = [self setDefaults];
+    }else{
+        /*
+         * Ya hay algo, es decir, en algun momento se guardó
+         * No hay nada en especial que hacer
+         */
+    }
+    
+    // Transformamos esas coordenadas del diccionariox  en un indexPath
+    indexPath = [NSIndexPath indexPathForRow:[[coords objectForKey: ROW_KEY] integerValue]
+                                   inSection:[[coords objectForKey: SECTION_KEY] integerValue]];
+    
+    // Devolvemos el vino en cuestion, le pasamos un indexPath y nos devuelve un vino
+    return [self wineForIndexPath:indexPath];
+}
+
+#pragma mark - Utils
+-(WineModel *)wineForIndexPath:(NSIndexPath *)indexPath{
+
+    //Averiguamos de que vino se trata
+    WineModel *wine = nil;
+    
+    if (indexPath.section == RED_WINE_SECTION) {
+        wine = [self.model redWineAtIndex:indexPath.row];
+    }else if (indexPath.section == WHITE_WINE_SECTION)
+    {
+        wine = [self.model whiteWineAtIndex:indexPath.row];
+    }else{
+        wine = [self.model otherWineAtIndex:indexPath.row];
+    }
+    return wine;
 }
 
 @end
